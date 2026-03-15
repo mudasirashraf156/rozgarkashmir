0f9ee5f59319dec9ee65d4abdfa821f6c1d5b192
Mudasir <mudasirashraf156@gmail.com>
2026-03-15 14:15:40 +0530

    updated

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: '/api' });
+const API = axios.create({ baseURL: 'https://rozgarkashmirbackend.onrender.com/api' });
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem('rk_token');
